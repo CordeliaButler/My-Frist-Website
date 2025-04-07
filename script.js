e342890438923a305feacbfa133cfe77314c3b52
CordeliaButler <Cordeliabutler11@gmail.com>
2025-04-06 21:50:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    document.getElementById("message").innerText = "Hey You, You clicked the button!";
+}
+function goToPage() {
+    window.location.href = "about.html";
+}
